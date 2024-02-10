7dc9ea3499e22353269dbeb847ec111f27f6d509
darkness132 <Igor.Maslennikov.3003@gmail.com>
2024-02-10 16:03:35 +0300

    added: new methods BuyArmor and BuyWeapon

--- ПР8. Гладиаторы/Player.cs
+++ ПР8. Гладиаторы/Player.cs
@@ -18,9 +18,12 @@ namespace ПР8.Гладиаторы
             glory = 0;
             gladiators = new List<Gladiator>()
             {
-                new Gladiator("Ваныш1"),
-                new Gladiator("Ваныш2"),
-                new Gladiator("Ваныш3")
+                new Gladiator("Аурелиан Железный Клинок"),
+                new Gladiator("Северус Кровавый Гром"),
+                new Gladiator("Леонид Сокрушитель"),
+                new Gladiator("Максимус Безжалостный"),
+                new Gladiator("Валерий Череполом"),
+                new Gladiator("Артемий Молотильщик")
             };
         }
 
@@ -34,6 +37,32 @@ namespace ПР8.Гладиаторы
             gladiator.Health = 100;
             money -= 100;
             Console.WriteLine($"Гладиатор {gladiator.Name} восстановил свое здоровье.");
+        }
+
+        internal void BuyArmor(Gladiator gladiator, Armor armor)
+        {
+            if (money < armor.Price)
+            {
+                Console.WriteLine("У вас недостаточно монет для покупки этого доспеха.");
+                return;
+            }
+
+            money -= armor.Price;
+            gladiator.Armor = armor;
+            Console.WriteLine($"Вы купили {armor.Name} за {armor.Price} монет.");
+        }
+
+        internal void BuyWeapon(Gladiator gladiator, Weapon weapon)
+        {
+            if (money < weapon.Price)
+            {
+                Console.WriteLine("У вас недостаточно денег для покупки этого оружия.");
+                return;
+            }
+
+            money -= weapon.Price;
+            gladiator.Weapon = weapon;
+            Console.WriteLine($"Вы купили {weapon.Name} за {weapon.Price} монет.");
         }
     }
 }
