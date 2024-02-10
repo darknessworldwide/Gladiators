4156d7a5567308fce694f62f0d8ca8c587031893
nyaama <bykovmaksim2006@gmail.com>
2024-02-10 14:25:49 +0300

    refactoring: added new method

--- ПР8. Гладиаторы/Player.cs
+++ ПР8. Гладиаторы/Player.cs
@@ -10,11 +10,23 @@ namespace ПР8.Гладиаторы
     {
         int money;
         int glory;
+        List<Gladiator> gladiators;
 
         internal Player()
         {
             money = 100;
             glory = 0;
+            gladiators = new List<Gladiator>()
+            {
+                new Gladiator("Ваныш1"),
+                new Gladiator("Ваныш2"),
+                new Gladiator("Ваныш3")
+            };
+        }
+
+        internal void HireGladiators(int number)
+        {
+            for (int i = 0; i < number; i++) { gladiators.Add(new Gladiator("Григорий")); }
         }
 
         internal void Heal(Gladiator gladiator)
